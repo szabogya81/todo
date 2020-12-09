e44b7e4d4ad82f225220900a8e591762eb6e3fff
RaiN <szabogya81@gmail.com>
2020-12-09 17:25:48 +0100

    JS code clean

--- js/main.js
+++ js/main.js
@@ -1,23 +1,16 @@
-let currentDate = new Date();
-let latestTaskId;
-const dayHeading = document.querySelector('.todo__day');
-const dateHeading = document.querySelector('.todo__date');
-dayHeading.textContent = 
-`${currentDate.toLocaleDateString('en-US', { weekday: 'long'})}`;
-dateHeading.textContent = 
-`${currentDate.toLocaleDateString('en-US', { day: '2-digit'})}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`;
-
-const addTaskInput = document.querySelector('.input--add');
 const addTaskBtn = document.querySelector('.btn--add');
+const addTaskInput = document.querySelector('.input--add');
+const clearButton = document.querySelector('.btn--clear');
+const completedItems = document.querySelector('.completed__percent');
+const dateHeading = document.querySelector('.todo__date');
+const dayHeading = document.querySelector('.todo__day');
+const pendigItems = document.querySelector('.pending__count');
+const showHideButton = document.querySelector('.btn--hide');
+const todoCompletedSection = document.querySelector('.todo__completed');
 const todoList = document.querySelector('.todo__pending .todoList');
 const todoListCompleted = document.querySelector('.todo__completed .todoList');
-const todoCompletedSection = document.querySelector('.todo__completed');
 
-const pendigItems = document.querySelector('.pending__count');
-const completedItems = document.querySelector('.completed__percent');
-const showHideButton = document.querySelector('.btn--hide');
-const clearButton = document.querySelector('.btn--clear');
-
+let latestTaskId;
 
 const storageHandler = {
     addTask(taskName) { return addTask(taskName); },
@@ -46,10 +39,9 @@ function deleteTasks() {
 }
 
 function getTasks() {
-    let taskKeys = Object.keys(localStorage).filter(key => key.startsWith('dTask_')).sort();
-    let taskArray = [];
-    taskKeys.forEach(key => taskArray.push([key, localStorage.getItem(key)]));
-    return taskArray;
+    let taskKeys = Object.keys(localStorage)
+        .filter(key => key.startsWith('dTask_')).sort().reverse();
+    return taskKeys.map(key => [key, localStorage.getItem(key)]);
 }
 
 function getLatestTaskId() {
@@ -64,10 +56,8 @@ function init() {
     latestTaskId = Number.parseInt(storageHandler.getLatestTaskId());
     if(!latestTaskId) { latestTaskId = 0; }
     updatePendingTasks();
-    setDeleteHandlers();
-    addTaskBtn.addEventListener('click', addNewTask);
-    showHideButton.addEventListener('click', showHideCompleted);
-    clearButton.addEventListener('click', deletePendingTasks);
+    setButtonEvenetHandlers();
+    setDateHeadings();
 }
 
 function setDeleteHandlers() {
@@ -75,25 +65,42 @@ function setDeleteHandlers() {
     deleteSpans.forEach(item => item.addEventListener('click', deletePendingTask));
 }
 
+function setButtonEvenetHandlers() {
+    setDeleteHandlers();
+    addTaskBtn.addEventListener('click', addNewTask);
+    showHideButton.addEventListener('click', showHideCompleted);
+    clearButton.addEventListener('click', deletePendingTasks);
+}
+
+function setDateHeadings() {
+    let currentDate = new Date();
+    dayHeading.textContent = 
+        `${currentDate.toLocaleDateString('en-US', { weekday: 'long'})}`;
+    dateHeading.textContent = 
+        `${currentDate.toLocaleDateString('en-US', 
+        { day: '2-digit'})}-${currentDate.getMonth() + 1}-${currentDate.getFullYear()}`;
+}
+
 function setTaskStatistics(pendingCount, completedCount = 0) {
     pendigItems.textContent = `You have ${pendingCount} pending item(s)`;
-    let completedPercent = completedCount > 0 ? (completedCount / (pendingCount + completedCount)) * 100 : 0;
+    let completedPercent = completedCount > 0 ? 
+        (completedCount / (pendingCount + completedCount)) * 100 : 0;
     completedItems.textContent = `Completed tasks: ${completedPercent.toFixed(2)}%`
 }
 
 function addNewTask() {
     if(addTaskInput.value.length > 0) {
-        storageHandler.addTask(addTaskInput.value);
+        let newTaskId = storageHandler.addTask(addTaskInput.value);
         addTaskInput.value = '';
         updatePendingTasks();
+        document.querySelector(`input[value=${newTaskId}]`)
+        .parentElement.classList.add('new');
     }  
 }
 
-function addTaskToList(task, isNew = false) {
-    let liInner = createTaskListItem(task[0], task[1]);
+function addTaskToList(task) {
     let liItem = document.createElement('li');
-    if(isNew) { liItem.classList.add('new') };
-    liItem.innerHTML = liInner;
+    liItem.innerHTML = createTaskListItem(task[0], task[1]);;
     todoList.appendChild(liItem);
 }
 
@@ -129,7 +136,7 @@ function showHideCompleted(e) {
     let buttonLabel = e.currentTarget.labels[0];
     buttonLabel.textContent = todoCompletedSection.classList.toggle('show') ? 
         'Hide Completed' : 'Show Completed';
-        
+
     setTaskStatistics(todoList.childElementCount, todoListCompleted.childElementCount);
 }
 
